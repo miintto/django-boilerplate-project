3e229cb9d6a2f599c10fb7cecf166f5e20a638d1
miintto <mjpark@datamarketing.co.kr>
2020-12-27 05:09:31 +0900

    20201227 feat: Separate a logging parameter into dev and prod

--- config/settings/base.py
+++ config/settings/base.py
@@ -114,38 +114,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
-
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "default": {
-            "format": "{message}",
-            "style": "{"
-        }
-    },
-    "handlers": {
-        "console": {
-            "level": "INFO",
-            "class": "logging.StreamHandler",
-            "formatter": "default"
-        },
-        "file": {
-            "level": "INFO",
-            "class": "logging.handlers.TimedRotatingFileHandler",
-            "filename": os.path.join(BASE_DIR, "logs/api_service.log"),
-            "when": "midnight",
-            "formatter": "default",
-            "encoding": "utf-8"
-        }
-    },
-    "loggers": {
-        "app_test": {
-            "handlers": ["file", "console"],
-            "level": "INFO",
-            "propagate": True
-        },
-    }
-}
--- config/settings/dev.py
+++ config/settings/dev.py
@@ -8,3 +8,36 @@ INSTALLED_APPS += [
 DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "format": "{message}",
+            "style": "{"
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+            "formatter": "default"
+        },
+        "file": {
+            "level": "INFO",
+            "class": "logging.handlers.TimedRotatingFileHandler",
+            "filename": os.path.join(BASE_DIR, "logs/api_service.log"),
+            "when": "midnight",
+            "formatter": "default",
+            "encoding": "utf-8"
+        }
+    },
+    "loggers": {
+        "app_test": {
+            "handlers": ["file", "console"],
+            "level": "INFO",
+            "propagate": True
+        },
+    }
+}
